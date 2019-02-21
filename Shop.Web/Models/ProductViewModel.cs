f6a3b06a7d8dcfa16f4c5a1e80f2379567ac9d26
José Donoso <jpdonosom@gmail.com>
2019-02-21 15:17:30 -0300

    Implementacion ProductViewModel

---
+++ Shop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Shop.Web.Models
+{
+    using Data.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Http;
+
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
