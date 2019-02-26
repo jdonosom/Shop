6abd3640d7e4746055e7deb71b8573511f3c07ec
José Donoso <jpdonosom@gmail.com>
2019-02-26 09:00:39 -0300

    Inplementando Login 1

--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,6 +1,41 @@
-﻿namespace Shop.UIForms.ViewModels
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Shop.UIForms.ViewModels
 {
     public class LoginViewModel
     {
+        public string  Email { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe ingresar su Email",
+                    "Aceptar");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debe ingresar su Password",
+                    "Aceptar");
+                return;
+            }
+            await Application.Current.MainPage.DisplayAlert(
+                    "OK",
+                    "Usted a ingresado correctamente",
+                    "Aceptar");
+
+        }
     }
 }
