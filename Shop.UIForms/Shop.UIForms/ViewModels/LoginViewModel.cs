849f3da4699e30518795f27a9005e5288d2ef9c3
José Donoso <jpdonosom@gmail.com>
2019-02-27 11:58:49 -0300

    Mostrar todos los productos en Xamarin Forms

--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight.Command;
+using Shop.UIForms.Views;
 using System;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -47,12 +48,8 @@ namespace Shop.UIForms.ViewModels
                 return;
             }
 
-
-            await Application.Current.MainPage.DisplayAlert(
-                    "OK",
-                    "Usted a ingresado correctamente",
-                    "Aceptar");
-
+            MainViewModel.GetInstance().Products = new ProductsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new ProductsPage());
         }
     }
 }
